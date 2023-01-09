1c6cc7618a2f640098a78652c79b6d622703836e
SylvainTorrenti <torrsyl@gmail.com>
2023-01-09 09:51:10 +0100

    Passage du paradigme procédurale à celui de l'objet
    Les changements suivent ont été realisés:
    -Création de la classe Rectangle
    -Encapsulation de la Largeur et de la Hauteur dans la nouvelle classe
    -Création de la méthode Surface dans la classe Rectangle
    -Modification du programme principale afin d'utiliser la classe Rectangle

---
+++ ApplicationRectangle/ApplicationRectangle.cs
@@ -0,0 +1,9 @@
+﻿using ApplicationRectangle;
+
+Rectangle Rect = new Rectangle();
+Rect.Largeur = 12.4;
+Rect.Hauteur = 5.6;
+
+Console.WriteLine($"Surface du rectangle {Rect.Surface()}");
+
+
---
+++ ApplicationRectangle/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationRectangle
+{
+    internal class Rectangle
+    {
+        public double Largeur;
+        public double Hauteur;
+
+        public double Surface()
+        {
+            return Largeur * Hauteur;
+        }
+    }
+}
