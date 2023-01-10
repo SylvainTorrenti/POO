b231b431344c728cd1c7f4ff6aa043ff8ef7105f
SylvainTorrenti <torrsyl@gmail.com>
2023-01-10 09:51:09 +0100

    Encapsulation des attributs de la Classe Rectangle
    -passage en private de Largeur et Hauteur
    -Création des accesseurs pour Largeur et Hauteur

--- ApplicationRectangle/ApplicationRectangle.cs
+++ ApplicationRectangle/ApplicationRectangle.cs
@@ -1,9 +1,23 @@
 ﻿using ApplicationRectangle;
 
-Rectangle Rect = new Rectangle();
-Rect.Largeur = 12.4;
-Rect.Hauteur = 5.6;
+#region Create recutangle
+Rectangle Rect1 = new Rectangle();
+Rect1.SetLargeur(12.5);
+Rect1.SetHauteur(5.5);
 
-Console.WriteLine($"Surface du rectangle {Rect.Surface()}");
+Rectangle Rect2 = new Rectangle();
+Rect2.SetLargeur(4);
+Rect2.SetHauteur(17.3);
+#endregion
+
+
+#region Use Get Set
+Console.WriteLine($"Hauteur du rectangle 1 {Rect1.GetHauteur()}");
+Console.WriteLine(Rect2.GetHauteur()); 
+#endregion
+#region Use method
+Console.WriteLine($"Surface du rectangle {Rect1.Surface()}");
+Console.WriteLine($"Surface du rectangle {Rect2.Surface()}"); 
+#endregion
 
 
--- ApplicationRectangle/Rectangle.cs
+++ ApplicationRectangle/Rectangle.cs
@@ -8,12 +8,52 @@ namespace ApplicationRectangle
 {
     internal class Rectangle
     {
-        public double Largeur;
-        public double Hauteur;
+        #region Attribute
 
+        private double Largeur;
+        private double Hauteur;
+
+        #endregion
+
+        #region Get & Set
+        public double GetLargeur()
+        {
+            return Largeur;
+        }
+        public void SetLargeur(double Largeur)
+        {
+            if (Largeur < 0)
+            {
+                Console.WriteLine("La valeur ne peut pas être <0");
+            }
+            else
+            {
+                this.Largeur = Largeur;
+            }
+
+        }
+        public double GetHauteur()
+        {
+            return Hauteur;
+        }
+        public void SetHauteur(double Hauteur)
+        {
+            if (Hauteur < 0)
+            {
+                Console.WriteLine("La valeur ne peut pas être <0");
+            }
+            else
+            {
+                this.Hauteur = Hauteur;
+            }
+
+        } 
+        #endregion
+        #region Method
         public double Surface()
         {
             return Largeur * Hauteur;
-        }
+        } 
+        #endregion
     }
 }
